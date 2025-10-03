cf3e66dbeaa9f3490879b9eb32284f39b1f58d9e
Abhishek0628 <abhijnv124@gmail.com>
2025-10-04 00:35:09 +0530

    modified

--- s5/ML lab 25/ML/matplotlib/marker.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-ypoints=np.array([1,2,3,8,19,2,34])
-# plt.plot(ypoints,marker='*')
-plt.plot(ypoints,marker='o')
-# plt.plot(ypoints,marker='*')
-# plt.plot(ypoints,marker='*')
-# plt.plot(ypoints,marker='*')
-plt.show()
