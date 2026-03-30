4c7bc03ae5e74c44146504bda52d89a0e1712e56
Abhishek0628 <abhijnv124@gmail.com>
2026-03-30 20:04:55 +0530

    modified

--- s3/pdlab/ASSG1_B220628CS_ABHISHEK/Q1/ASSG1_B220628CS_ABHISHEK_1.py
+++ s3/pdlab/ASSG1_B220628CS_ABHISHEK/Q1/ASSG1_B220628CS_ABHISHEK_1.py
@@ -22,3 +22,19 @@ Sample Output 2:
 '''
 
 # solution
+class solution:
+    def count(self,str,ch):
+        c=0
+        for i in range(len(str)):
+            if str[i]==ch:
+                c+=1
+        return c
+
+if __name__=="__main__":
+    s=solution()
+    # str=map(char,input().split())
+    str=input()
+    ch=input()
+    c=s.count(str,ch)
+    print(c)
+
