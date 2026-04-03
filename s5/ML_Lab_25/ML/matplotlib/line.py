0ec8998f850ba00dbae8dfe8c53b83a060ca9b4f
Abhishek0628 <abhijnv124@gmail.com>
2026-04-03 18:59:16 +0530

    modified

--- s5/ML_Lab_25/ML/matplotlib/line.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-ypoints=np.array([1,2,3,4,7,9,12])
-plt.plot(ypoints,linestyle='dashed',color='r',linewidth='10')
-plt.show()
--- s5/ML_Lab_25/ML/numpy/reshape.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-a=np.array([1,2,3,4,5,6])
-a.reshape(2,-1)
-a.reshape(2,3)# rows*column=no of element
-print(a)
